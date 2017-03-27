185ea4301bffaec02081404becb455c19adfb79e
Unknown <425970872@qq.com>
2017-03-27 10:22:28 +0800

    用HW这个版本的代码

---
+++ HW/build/CMakeFiles/cdn.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cdn.dir/cdn.cpp.o"
+  "CMakeFiles/cdn.dir/deploy.cpp.o"
+  "CMakeFiles/cdn.dir/io.cpp.o"
+  "/home/chenyunfei/HW/bin/cdn.pdb"
+  "/home/chenyunfei/HW/bin/cdn"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cdn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
